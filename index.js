1538087e98cac8bc5a3d65d25c12173002a9932b
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-24 23:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ return string.toUpperCase()
    console.log(string.toLowerCase())
  }
 
-function sayHitoGrandma(string) {
+function sayHitoGrandma(string) 
+{
    if (string.toLowerCase() === string) {
      return "I can\'t hear you!"
    } else if (string.toUpperCase() === string) {
      return "YES INDEED!" 
    } else if (string === "I love you, Grandma.") {
-     return "I love you, too.";
-   }
+     return "I love you, too."
+   }}
